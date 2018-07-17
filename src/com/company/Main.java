d45b06669a1dc86d27ac9b063c160b4ddbe4ce32
zsivany <tamaspapp84@gmail.com>
2018-07-17 14:11:38 +0200

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Saveable project from Udemy!");
+	// write your code here
+    }
+}
